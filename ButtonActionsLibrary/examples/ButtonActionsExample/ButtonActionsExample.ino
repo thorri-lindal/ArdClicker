4d11b60e09249dfffd6b1d810bbc07bac087466a
Þorri Líndal Guðnason <jormunvakur@tuta.io>
2025-02-04 22:41:40 +0000

    fixed naming

--- ButtonActionsLibrary/examples/ButtonActionsExample/ButtonActionsExample.ino
+++ ButtonActionsLibrary/examples/ButtonActionsExample/ButtonActionsExample.ino
@@ -1,28 +1,31 @@
+#include <ArdClicker.h>
+
+ArdClicker button(2);
+
 void setup() {
   Serial.begin(9600);
   
   // Initialize the button actions library
-  ButtonActions buttonActions;
-  buttonActions.begin(2); // Pin 2 for the button
+  button.begin();
 
   // Add actions for different button presses
-  buttonActions.addAction(1, []() {
+  button.addAction([]() {
     Serial.println("Action for 1st press");
   });
   
-  buttonActions.addAction(2, []() {
+  button.addAction([]() {
     Serial.println("Action for 2nd press");
   });
   
-  buttonActions.addAction(3, []() {
+  button.addAction([]() {
     Serial.println("Action for 3rd press");
   });
   
-  buttonActions.addAction(4, []() {
+  button.addAction([]() {
     Serial.println("Action for 4th press");
   });
 }
 
 void loop() {
-  buttonActions.update(); // Update the button state and trigger actions
+  button.update(); // Update the button state and trigger actions
 }
